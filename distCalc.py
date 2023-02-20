0c9c14bdf205d1da7e3507977f3036f0a406437d
Lukas-Quader <62140068+Lukas-Quader@users.noreply.github.com>
2023-02-20 18:28:07 +0100

    Update distCalc.py

--- distCalc.py
+++ distCalc.py
@@ -8,8 +8,10 @@ def rad2deg(radians):
 def deg2rad(degrees):
     radians = degrees * pi / 180
     return radians
+
+
 # Die Funktion gibt die Entfernung zwischen zwei Koordinaten in Meter zurück
-def getDistanceBetweenPointsNew(latitude1, longitude1, latitude2, longitude2):
+def getDistance(latitude1, longitude1, latitude2, longitude2):
     
     theta = longitude1 - longitude2
     
@@ -21,33 +23,34 @@ def getDistanceBetweenPointsNew(latitude1, longitude1, latitude2, longitude2):
     )
     return round(distance * 1609.344, 3) 
 
+
 # Die Funktion gibt ein Tupel mit den Breiten- und Längengraden der Zielkoordinate in Dezimalgrad zurück.
 # latitude: die Breitengrad-Koordinate der Startposition in Dezimalgrad
 # longitude: die Längengrad-Koordinate der Startposition in Dezimalgrad
 # bearing: die Richtung in Grad, in die das Objekt reist (0 Grad ist Norden, 90 Grad ist Osten)
 # speed: die Geschwindigkeit des Objekts in km/h
 # time: die Zeitdauer, für die das Objekt reist, in Sekunden
-def getDestinationCoordinates(latitude, longitude, bearing, speed, time):
-    R = 6371  # Earth's radius in km
+def getDestination(latitude, longitude, bearing, speed, time):
+    R = 6370.693  # Erdradius in km
     
-    # convert latitude and longitude to radians
+    # umwandeln von Breitengrad und Längengrad in Radiant
     lat1 = radians(latitude)
     lon1 = radians(longitude)
     
-    # convert bearing to radians
+    # umwandeln der Richtung in Radiant
     bearing = radians(bearing)
     
-    # convert speed to km/s
+    # umwandeln von km/h zu km/s
     speed = speed / 3600
     
-    # calculate distance
+    # distance berechnen
     distance = speed * time
     
-    # calculate new latitude and longitude
+    # berechnen der neuen latitude und longitude
     lat2 = asin(sin(lat1) * cos(distance/R) + cos(lat1) * sin(distance/R) * cos(bearing))
     lon2 = lon1 + atan2(sin(bearing) * sin(distance/R) * cos(lat1), cos(distance/R) - sin(lat1) * sin(lat2))
     
-    # convert back to degrees
+    # zurück zu degrees umwandeln
     lat2 = degrees(lat2)
     lon2 = degrees(lon2)
     
@@ -56,15 +59,20 @@ def getDestinationCoordinates(latitude, longitude, bearing, speed, time):
 
 def main(args=None):
     lat1 = 53.870004
-    lat2 = 53.86961705908918
     lon1 = 10.699840
-    lon2 = 11.004974481161332
-    dist = getDistanceBetweenPointsNew(lat1, lon1, lat2, lon2)
-    newPosition = getDestinationCoordinates(lat1, lon1, 90, 20, 3600)
-
+    newPosition = getDestination(lat1, lon1, 90, 20, 3600)
+    lat2 = newPosition[0]
+    lon2 = newPosition[1]
+    dist = getDistance(lat1, lon1, lat2, lon2)
+    
+    for x in range(6): 
+       newPosition = getDestination(lat1, lon1, 90 + 10 * x, 10 * x, 1) 
+       print(newPosition)
 
     print(str(dist) + ' m')
     print(newPosition)
+
+
 
 if __name__ == '__main__':
     main()
