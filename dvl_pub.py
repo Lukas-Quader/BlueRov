b88a4b43e18fed9be8ca26a6467033e0f10c5c10
Lukas-Quader <62140068+Lukas-Quader@users.noreply.github.com>
2023-03-09 14:39:27 +0100

    Update dvl_pub.py

--- dvl_pub.py
+++ dvl_pub.py
@@ -10,15 +10,24 @@ class DvlPublisherNode(Node):
         self.publisher_ = self.create_publisher(String, 'dvl_data', 10)
         self.timer_ = self.create_timer(0.1, self.timer_callback)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect(('localhost', 16171))
+        self.sock.connect(('192.168.178.52', 9002))
 
     def timer_callback(self):
         data = self.sock.recv(1024)
-        json_data = json.loads(data)
-        msg = String()
-        msg.data = json.dumps(json_data)
-        self.publisher_.publish(msg)
-        self.get_logger().info('Published: "%s"' % msg.data)
+        decoder = json.JSONDecoder()
+        pos = 0
+        while True:
+            try:
+                json_data, pos = decoder.raw_decode(data[pos:])
+                
+                msg = String()
+                msg.data = json.dumps(json_data)
+                self.publisher_.publish(msg)
+                self.get_logger().info('Published: "%s"' % msg.data)
+            except json.JSONDecodeError as e:
+                # Wenn ein JSONDecodeError auftritt, bricht die Schleife ab
+                # und wartet auf weitere Daten, um das JSON-Objekt zu vervollständigen.
+                break
 
 def main(args=None):
     rclpy.init(args=args)
