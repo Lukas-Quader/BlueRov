45fc01884b179a5ebcf9963241649649f80ea405
Lukas-Quader <62140068+Lukas-Quader@users.noreply.github.com>
2023-03-15 20:01:32 +0100

    Update dvl_pub.py

--- dvl_pub.py
+++ dvl_pub.py
@@ -1,10 +1,15 @@
 import rclpy
+#import flags.py
 import socket
 import json
+import time
 from rclpy.node import Node
 from std_msgs.msg import String # Übertragung der Daten im Format String
 
-
+MSG_COUNT = False
+MSG_XYZ = False
+MSG_RAW_TS = False
+MSG_RAW = True
 
 
 def main(args=None):
@@ -14,24 +19,41 @@ def main(args=None):
 
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Bind and listen
-    serverSocket.bind(('192.168.194.95', 16171))
-    serverSocket.listen()
-    (clientConnected, clientAddress) = serverSocket.accept()
+    
+    serverSocket.connect(("192.168.194.95", 16171))
+    
     msg = String()
+    counter = 0
 
     while True:     
-        dataFromClient = clientConnected.recv(2048)
-                        
+        dataFromClient = serverSocket.recv(2048)
+        #print(flags.test)
         try:            
             decoder = json.JSONDecoder()
             if dataFromClient:
                 decode_data = decoder.raw_decode(dataFromClient.decode())
-                if 'ts' in decode_data[0]:                    
-                    msg.data = "x: " + str(decode_data[0]["x"]) +" y: " + str(decode_data[0]["y"]) + " z: " + str(decode_data[0]["z"])
+                if MSG_RAW: 
+                    msg.data = str(decode_data)
                     dvl_pub.publish(msg)
+                    print(msg)
+                elif 'ts' in decode_data[0]:                    
+                    counter += 1
+                    if MSG_XYZ:
+                        msg.data = "x: " + str(decode_data[0]["x"]) +" y: " + str(decode_data[0]["y"]) + " z: " + str(decode_data[0]["z"]) 
+                    if MSG_COUNT:
+                        msg.data += ' Counter: ' + str(counter) 
+                    if MSG_RAW_TS:
+                        msg.data = str(decode_data)
+                    dvl_pub.publish(msg)
+                    print(msg)
+                    #time.sleep(1)
+            else: 
+                msg.data = 'dvl_timeout'
+                dvl_pub.publish(msg)
+                print('sendet:' + msg)
         except json.JSONDecodeError as e:
             print("FAILED" + dataFromClient.decode())
             continue
-
+        
 if __name__ == '__main__':
     main()
